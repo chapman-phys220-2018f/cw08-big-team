7c82171741c47e101be295244222efe03ac498c7
jsavage@chapman.edu <jsavage@chapman.edu>
2018-11-01 22:47:28 +0000

    ensuring modules pass travis

---
+++ sinesum.py
@@ -0,0 +1,69 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+###
+# Name: Jack Savage
+# Student ID: 2295072 
+# Email: jsavage@chapman.edu
+# Course: PHYS220/MATH220/CPSC220 Fall 2018 Assignment: CW03
+###
+
+'''
+MODULE DOCSTRING
+'''
+import math
+import numpy as np
+
+def s(a, n, T = 2*math.pi):
+    '''
+    Computes sum of equation given in readme
+    
+    Args:
+        a (float): when multiplied by "T" gives "t" value used in equation
+        n (int): term limit for sequence
+        T (float): used in equation and in generating "t" value
+    Returns:
+        value (float): computed sum value
+    '''
+    # range of "k" values
+    k = np.arange(1,n+1,1)
+    t = a*T
+    def compute_term(k, t, T = 2*math.pi):
+        '''
+        Computes the value of "k" using given function in readme
+        
+        Args:
+            k (int/float): "k" value to be evaluated
+            t (int/float): "t" value used in equation: defined in outer function
+            T (float): "T" value used in equation: defined in outer function
+            
+        Returns:
+            raw value (float): Value of function at "k"
+        '''
+        return (1/(2*k-1))*math.sin(((2*(2*k-1)*math.pi*t))/T)
+        
+    # evaluating function on domain array
+    f = np.vectorize(compute_term)
+    k = f(k,t)
+    
+    # multiplying sum of range array with constant to determine final vlaue
+    value = (4/math.pi)*k.sum()
+    return value
+
+def f(t):
+    '''
+    Implementation of f(t) function given in readme
+    
+    Args:
+        t (float): variable used in expression found in readme
+    Returns:
+        f(t) (int): function in readme evaluated at "t"
+    '''
+    if t == 0:
+        return 0
+    elif t < math.pi and t > 0.0:
+        return 1
+    elif t > -1*math.pi and t < 0.0:
+        return -1
+    else:
+        return False
--- test_sinesum.py
+++ test_sinesum.py
@@ -6,7 +6,14 @@ Verify implementation of the Fourier sine series using numpy arrays.
 """
 
 import sinesum
+import math
 
-def test_dummy():
-    """Dummy test that always fails. Please replace with actual tests."""
-    assert False, "This is a dummy test"
+def test_s():
+    """With given inputs, "s" function should equal sin(1)"""
+    assert math.isclose((sinesum.s(1/(2*math.pi),1)/(4/math.pi)), math.sin(1)), "s function not working properly"
+
+def test_f():
+    """Ensures each use case is working correctly"""
+    assert sinesum.f(1) == 1, "Not working for cases > 0"
+    assert sinesum.f(-1) == -1, "Not working for cases < 0"
+    assert sinesum.f(0) == 0, "Not working for t = 0"
